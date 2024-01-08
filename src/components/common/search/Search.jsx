4abb4bb8a4f227d8168a813d5d66cde1929bb5e0
aristotleinigo9 <99675563+aristotleinigo9@users.noreply.github.com>
2024-01-08 10:03:48 +0300

    Update Search.jsx

--- src/components/common/search/Search.jsx
+++ src/components/common/search/Search.jsx
@@ -3,7 +3,7 @@ import "./search.css"
 
 const Search = () => {
   return (
-    <div className='search' ><a href="https://itd-course-catalogue-full-list.netlify.app/">
+    <div className='search' ><a href="https://itd-courses.vercel.app">
     <button>
       Click here to view full catalogue!
     </button>
