87f50067dcda13e9731ca6f02ba255c6d0660823
aristotleinigo9 <99675563+aristotleinigo9@users.noreply.github.com>
2024-01-14 10:21:30 +0300

    Update dummydata.js

--- src/dummydata.js
+++ src/dummydata.js
@@ -3,11 +3,11 @@
     {
       id: 1,
       cover: "../images/courses/c1.png",
-      coursesName: "Industrial Math",
+      coursesName: "Basic Mathematics",
       courseInfo: [
         {
-          description: "Topics include fractions, decimals, percents, accuracy and precision of measurements, basic statistics, geometry, and trigonometry as applied in job-related scenarios.",
-          learners: "APNE, VCGNE, RFT, SST (60 hrs)",
+          description: "General mathematics course aimed at developing proficiency in basic arithmetic computational skills and relating these skills to careers and real-life situations",
+          learners: "APNE, VCGNE, SST (90 hrs)",
         },
       ],
 
@@ -15,11 +15,11 @@
 
     {
       id: 2,
-      cover: "../images/courses/c2.png",
-      coursesName: "English Functional ",
+      cover: "../images/courses/c1.png",
+      coursesName: "Technical Mathematics",
       courseInfo: [
         {
-          description: "Topics include fractions, decimals, percents, accuracy and precision of measurements, basic statistics, geometry, and trigonometry as applied in job-related scenarios.",
+          description: "Elementary technical mathematics course aimed at developing proficiency in job-related mathematical concepts and applications to trainees who will hold jobs, such as craft, technical, and operator, which will require skills beyong basic mathematics.",
           learners: "APNE, VCGNE, RFT, SST (60 hrs)",
         },
       ],
@@ -31,8 +31,20 @@
       coursesName: "Computer Literacy & Online Security Essentials",
       courseInfo: [
         {
-          description: "Topics include fractions, decimals, percents, accuracy and precision of measurements, basic statistics, geometry, and trigonometry as applied in job-related scenarios.",
+          description: "This covers essential concepts and skills relating to computer devices, file management, networks, information, and online security. Learners are expected to use Microsoft (MS) Office programs for business and personal use.",
           learners: "APNE, VCGNE (30 hrs)",
+        },
+      ],
+    },
+
+     {
+      id: 4,
+      cover: "../images/courses/c2.png",
+      coursesName: "English Functional",
+      courseInfo: [
+        {
+          description: "With levels ranging from 2 to 6, these courses offer foundation instruction in functional English language skills, with focus on development of productive skills (writing and speaking) that facilitate effective verbal communication accross all functional areas. Also provided are foundational instuction covering writing, typing, listening, and reading skills, with emphasis on language mastery needed to study at the ITC and to perform job tasks effectively.",
+          learners: "APNE, VCGNE, RFT, STT (175 hrs to 230 hrs)",
         },
       ],
     },
