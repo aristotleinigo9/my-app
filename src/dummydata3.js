e2fd577b008ff5b99b36e3af9e3168dc90964a32
aristotleinigo9 <99675563+aristotleinigo9@users.noreply.github.com>
2024-01-15 12:47:31 +0300

    Update dummydata3.js

--- src/dummydata3.js
+++ src/dummydata3.js
@@ -3,11 +3,11 @@ export const coursesCard = [
   {
     id: 1,
     cover: "../images/courses/c8.png",
-    coursesName: "Crane Operator",
+    coursesName: "Crane Operator Certification",
     courseInfo: [
       {
-        description: "Topics include fractions, decimals, percents, accuracy and precision of measurements, basic statistics, geometry, and trigonometry as applied in job-related scenarios.",
-        learners: "APNE, VCGNE, RFT, SST (60 hrs)",
+        description: "To test and certify crane operators on new types of cranes, grove GMK 3050, GMK 5100, GMK 5200 and Liebher LTM 1080.",
+        learners: "Regulars, Contractors (8 hrs)",
       },
     ],
 
@@ -16,11 +16,11 @@ export const coursesCard = [
   {
     id: 2,
     cover: "../images/courses/c8.png",
-    coursesName: "Rigger",
+    coursesName: "Manlift Operator Certification Test",
     courseInfo: [
       {
-        description: "Topics include fractions, decimals, percents, accuracy and precision of measurements, basic statistics, geometry, and trigonometry as applied in job-related scenarios.",
-        learners: "APNE, VCGNE, RFT, SST (60 hrs)",
+        description: "This is a test to certify operators on Manlift Operation. Operator will operate a Manlift (Hydraulic, Scissors, Telescoping). The operator will complete the tasks withour error and in accordance with the Company's operating procedures and Manlift manufacturer specification.",
+        learners: "Regulars, Contractors (8 hrs)",
       },
     ],
 
