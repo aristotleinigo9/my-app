8b84078a2ada8077687d1a320ec892101ed02113
aristotleinigo9 <99675563+aristotleinigo9@users.noreply.github.com>
2024-01-15 12:33:15 +0300

    Update dummydata2.js

--- src/dummydata2.js
+++ src/dummydata2.js
@@ -6,8 +6,8 @@ export const coursesCard = [
     coursesName: "Lock Out Tag Out",
     courseInfo: [
       {
-        description: "Topics include fractions, decimals, percents, accuracy and precision of measurements, basic statistics, geometry, and trigonometry as applied in job-related scenarios.",
-        learners: "APNE, VCGNE, RFT, SST (60 hrs)",
+        description: "At the end of this course, participants will be able to identify equipment, energy, and/or systems that require the application of LOTO, and apply the correct isolation methods before work can be performed.",
+        learners: "Regulars, Contractors (8 hrs)",
       },
     ],
 
@@ -16,11 +16,24 @@ export const coursesCard = [
   {
     id: 2,
     cover: "../images/courses/c7.png",
-    coursesName: "Work Permit Receiver",
+    coursesName: "Gas Testing",
     courseInfo: [
       {
-        description: "Topics include fractions, decimals, percents, accuracy and precision of measurements, basic statistics, geometry, and trigonometry as applied in job-related scenarios.",
-        learners: "APNE, VCGNE, RFT, SST (60 hrs)",
+        description: "Participants will learn about common gases and vapors in hydrocarbon facilities, some of their properties and hazardous effects, the requirements of the Company's gas testing program, equipment and procedures used, including responsibilities of a certified gas tester and be able to demonstrate the use of given gas monitor for work permit and operational requirements including its setup, recording of results and its operational maintenance and take gas tester's cetification test.",
+        learners: "Regulars, Contractors (8 hrs)",
+      },
+    ],
+
+  },
+
+   {
+    id: 3,
+    cover: "../images/courses/c7.png",
+    coursesName: "Hazard Recognition and Control",
+    courseInfo: [
+      {
+        description: "Participants will explore the elements of an effective hazard recognition and control program, discuss the steps in hazard and recognition and control process, recognize common types of hazards associated with a facility, process or work activity and to identify and implemet measures to control hazards.",
+        learners: "Regulars, Contractors (16 hrs)",
       },
     ],
 
