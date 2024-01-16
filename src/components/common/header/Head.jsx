6d61bced7b3e810108f0eebb21de2a0219570d3e
aristotleinigo9 <99675563+aristotleinigo9@users.noreply.github.com>
2024-01-16 09:58:50 +0300

    Update Head.jsx

--- src/components/common/header/Head.jsx
+++ src/components/common/header/Head.jsx
@@ -7,8 +7,6 @@ const Head = () => {
         <div className='container flexSB'>
           <div className='logo'>
             <h1>Industrial Training Department</h1>
-            <span>Center of Vocational Excellence</span>
-            <span>Transforming Lives</span>
           </div>
         </div>
       </section>
