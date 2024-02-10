6fc09d090b643cadca32ac122158991451a0cc8d
aristotleinigo9 <99675563+aristotleinigo9@users.noreply.github.com>
2024-02-10 20:45:25 +0300

    Update dummydata.js

--- src/dummydata.js
+++ src/dummydata.js
@@ -2,7 +2,7 @@
     {
       id: 1,
       cover: "../images/courses/c1.png",
-      coursesName: “Lorem Ipsum,
+      coursesName: “Lorem Ipsum",
       courseInfo: [
         {
           description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
