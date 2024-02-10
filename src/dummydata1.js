18c706bf7a48626a617d36f1f736ff30ab6a84cd
aristotleinigo9 <99675563+aristotleinigo9@users.noreply.github.com>
2024-02-10 20:47:10 +0300

    Update dummydata1.js

--- src/dummydata1.js
+++ src/dummydata1.js
@@ -1,55 +1,69 @@
-
-export const coursesCard = [
-  {
-    id: 1,
-    cover: "../images/courses/c4.png",
-    coursesName: "Basic Operator Course",
-    courseInfo: [
-      {
-        description: "This course is an introduction to the oil and gas industry. It provides basic information required to continue job skills training for any outside operation training pattern offered by the traininig center.",
-        learners: "APNE, VCGNE (300 hrs)",
-      },
-    ],
-
-  },
-
-  {
-    id: 2,
-    cover: "../images/courses/c4.png",
-    coursesName: "Refinery Basic Operator Course",
-    courseInfo: [
-      {
-        description: "Divided into two phases, this course introduces trainees to the refinery and train them in commmon operator tasks, safety procedures, and emergency procedures.",
-        learners: "APNE, VCGNE (400 hrs)",
-      },
-    ],
-  },
+  export const coursesCard = [
+    {
+      id: 1,
+      cover: "../images/courses/c2.png",
+      coursesName: “Lorem Ipsum”,
+      courseInfo: [
+        {
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+          learners: “LOREM, IPSUM, DOLOR (90 hrs)",
+        },
+      ],
+    },
+  ]
 
 
-  {
-    id: 3,
-    cover: "../images/courses/c4.png",
-    coursesName: "Electrical Hazard Recognition",
-    courseInfo: [
-      {
-        description: "This is for any employee or contractor working/maintaining electrical equipment or systems that operate on a voltage above 240 V. The course highlights the hazards of an arcflash and details safety procedures and precautions that should be observed when performing maintenance on equipment and systems that operate at these voltages.",
-        learners: "Regulars, Contractors (16 hrs)",
-      },
-    ],
-  },
+// export const coursesCard = [
+//   {
+//     id: 1,
+//     cover: "../images/courses/c4.png",
+//     coursesName: "Basic Operator Course",
+//     courseInfo: [
+//       {
+//         description: "This course is an introduction to the oil and gas industry. It provides basic information required to continue job skills training for any outside operation training pattern offered by the traininig center.",
+//         learners: "APNE, VCGNE (300 hrs)",
+//       },
+//     ],
 
-  {
-    id: 4,
-    cover: "../images/courses/c4.png",
-    coursesName: "Restructured APNE Machinist Program",
-    courseInfo: [
-      {
-        description: "The machinist core program provides theoretical knowledge and hands-on practical skills training for entry level into the targeted job.",
-        learners: "APNE (1945 hrs)",
-      },
-    ],
-  },
+//   },
 
-]
+//   {
+//     id: 2,
+//     cover: "../images/courses/c4.png",
+//     coursesName: "Refinery Basic Operator Course",
+//     courseInfo: [
+//       {
+//         description: "Divided into two phases, this course introduces trainees to the refinery and train them in commmon operator tasks, safety procedures, and emergency procedures.",
+//         learners: "APNE, VCGNE (400 hrs)",
+//       },
+//     ],
+//   },
+
+
+//   {
+//     id: 3,
+//     cover: "../images/courses/c4.png",
+//     coursesName: "Electrical Hazard Recognition",
+//     courseInfo: [
+//       {
+//         description: "This is for any employee or contractor working/maintaining electrical equipment or systems that operate on a voltage above 240 V. The course highlights the hazards of an arcflash and details safety procedures and precautions that should be observed when performing maintenance on equipment and systems that operate at these voltages.",
+//         learners: "Regulars, Contractors (16 hrs)",
+//       },
+//     ],
+//   },
+
+//   {
+//     id: 4,
+//     cover: "../images/courses/c4.png",
+//     coursesName: "Restructured APNE Machinist Program",
+//     courseInfo: [
+//       {
+//         description: "The machinist core program provides theoretical knowledge and hands-on practical skills training for entry level into the targeted job.",
+//         learners: "APNE (1945 hrs)",
+//       },
+//     ],
+//   },
+
+// ]
 
 
