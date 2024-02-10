df5deae4f1e367fbab78b34a2bc81fb531a2e021
aristotleinigo9 <99675563+aristotleinigo9@users.noreply.github.com>
2024-02-10 21:29:34 +0300

    Update Head.jsx

--- src/components/common/header/Head.jsx
+++ src/components/common/header/Head.jsx
@@ -6,7 +6,7 @@ const Head = () => {
       <section className='head'>
         <div className='container flexSB'>
           <div className='logo'>
-            <h1>Site Under Development</h1>
+            <h1>Site's Name Here</h1>
           </div>
         </div>
       </section>
