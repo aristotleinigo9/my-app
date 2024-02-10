179eb75bc33455691c5f44505e65b16623b04cc1
aristotleinigo9 <99675563+aristotleinigo9@users.noreply.github.com>
2024-02-10 20:49:40 +0300

    Update dummydata2.js

--- src/dummydata2.js
+++ src/dummydata2.js
@@ -1,44 +1,60 @@
+  export const coursesCard = [
+    {
+      id: 1,
+      cover: "../images/courses/c3.png",
+      coursesName: “Lorem Ipsum”,
+      courseInfo: [
+        {
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+          learners: “LOREM, IPSUM, DOLOR (90 hrs)",
+        },
+      ],
+    },
+  ]
 
-export const coursesCard = [
-  {
-    id: 1,
-    cover: "../images/courses/c7.png",
-    coursesName: "Lock Out Tag Out",
-    courseInfo: [
-      {
-        description: "At the end of this course, participants will be able to identify equipment, energy, and/or systems that require the application of LOTO, and apply the correct isolation methods before work can be performed.",
-        learners: "Regulars, Contractors (8 hrs)",
-      },
-    ],
 
-  },
 
-  {
-    id: 2,
-    cover: "../images/courses/c7.png",
-    coursesName: "Gas Testing",
-    courseInfo: [
-      {
-        description: "Participants will learn about common gases and vapors in hydrocarbon facilities, some of their properties and hazardous effects, the requirements of the Company's gas testing program, equipment and procedures used, including responsibilities of a certified gas tester and be able to demonstrate the use of given gas monitor for work permit and operational requirements including its setup, recording of results and its operational maintenance and take gas tester's cetification test.",
-        learners: "Regulars, Contractors (8 hrs)",
-      },
-    ],
 
-  },
+// export const coursesCard = [
+//   {
+//     id: 1,
+//     cover: "../images/courses/c7.png",
+//     coursesName: "Lock Out Tag Out",
+//     courseInfo: [
+//       {
+//         description: "At the end of this course, participants will be able to identify equipment, energy, and/or systems that require the application of LOTO, and apply the correct isolation methods before work can be performed.",
+//         learners: "Regulars, Contractors (8 hrs)",
+//       },
+//     ],
 
-   {
-    id: 3,
-    cover: "../images/courses/c7.png",
-    coursesName: "Hazard Recognition and Control",
-    courseInfo: [
-      {
-        description: "Participants will explore the elements of an effective hazard recognition and control program, discuss the steps in hazard and recognition and control process, recognize common types of hazards associated with a facility, process or work activity and to identify and implemet measures to control hazards.",
-        learners: "Regulars, Contractors (16 hrs)",
-      },
-    ],
+//   },
 
-  },
+//   {
+//     id: 2,
+//     cover: "../images/courses/c7.png",
+//     coursesName: "Gas Testing",
+//     courseInfo: [
+//       {
+//         description: "Participants will learn about common gases and vapors in hydrocarbon facilities, some of their properties and hazardous effects, the requirements of the Company's gas testing program, equipment and procedures used, including responsibilities of a certified gas tester and be able to demonstrate the use of given gas monitor for work permit and operational requirements including its setup, recording of results and its operational maintenance and take gas tester's cetification test.",
+//         learners: "Regulars, Contractors (8 hrs)",
+//       },
+//     ],
 
-]
+//   },
+
+//    {
+//     id: 3,
+//     cover: "../images/courses/c7.png",
+//     coursesName: "Hazard Recognition and Control",
+//     courseInfo: [
+//       {
+//         description: "Participants will explore the elements of an effective hazard recognition and control program, discuss the steps in hazard and recognition and control process, recognize common types of hazards associated with a facility, process or work activity and to identify and implemet measures to control hazards.",
+//         learners: "Regulars, Contractors (16 hrs)",
+//       },
+//     ],
+
+//   },
+
+// ]
 
 
