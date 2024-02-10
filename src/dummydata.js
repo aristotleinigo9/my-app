fb0f1235c3499d0f1e73d6bb72bce10b795b29a2
aristotleinigo9 <99675563+aristotleinigo9@users.noreply.github.com>
2024-02-10 20:41:03 +0300

    Update dummydata.js

--- src/dummydata.js
+++ src/dummydata.js
@@ -1,56 +1,70 @@
-
   export const coursesCard = [
     {
       id: 1,
       cover: "../images/courses/c1.png",
-      coursesName: "Basic Mathematics",
+      coursesName: “Lorem Ipsum,
       courseInfo: [
         {
-          description: "General mathematics course aimed at developing proficiency in basic arithmetic computational skills and relating these skills to careers and real-life situations.",
-          learners: "APNE, VCGNE, SST (90 hrs)",
-        },
-      ],
-
-    },
-
-    {
-      id: 2,
-      cover: "../images/courses/c1.png",
-      coursesName: "Technical Mathematics",
-      courseInfo: [
-        {
-          description: "Elementary technical mathematics course aimed at developing proficiency in job-related mathematical concepts and applications to trainees who will hold jobs, such as craft, technical, and operator, which will require skills beyond basic mathematics.",
-          learners: "APNE, VCGNE, RFT, SST (60 hrs)",
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+          learners: “LOREM, IPSUM, DOLOR (90 hrs)",
         },
       ],
     },
-
-    {
-      id: 3,
-      cover: "../images/courses/c3.png",
-      coursesName: "Computer Literacy & Online Security Essentials",
-      courseInfo: [
-        {
-          description: "This covers essential concepts and skills relating to computer devices, file management, networks, information, and online security. Learners are expected to use Microsoft (MS) Office programs for business and personal use.",
-          learners: "APNE, VCGNE (30 hrs)",
-        },
-      ],
-    },
-
-     {
-      id: 4,
-      cover: "../images/courses/c2.png",
-      coursesName: "English Functional",
-      courseInfo: [
-        {
-          description: "With levels ranging from 2 to 6, these courses offer foundation instruction in functional English language skills, with focus on development of productive skills (writing and speaking) that facilitate effective verbal communication accross all functional areas. Also provided are foundational instruction covering writing, typing, listening, and reading skills, with emphasis on language mastery needed to study at the ITC and to perform job tasks effectively.",
-          learners: "APNE, VCGNE, RFT, SST (175 hrs to 230 hrs)",
-        },
-      ],
-    },
-  
-  
   ]
+
+
+  // export const coursesCard = [
+  //   {
+  //     id: 1,
+  //     cover: "../images/courses/c1.png",
+  //     coursesName: "Basic Mathematics",
+  //     courseInfo: [
+  //       {
+  //         description: "General mathematics course aimed at developing proficiency in basic arithmetic computational skills and relating these skills to careers and real-life situations.",
+  //         learners: "APNE, VCGNE, SST (90 hrs)",
+  //       },
+  //     ],
+
+  //   },
+
+  //   {
+  //     id: 2,
+  //     cover: "../images/courses/c1.png",
+  //     coursesName: "Technical Mathematics",
+  //     courseInfo: [
+  //       {
+  //         description: "Elementary technical mathematics course aimed at developing proficiency in job-related mathematical concepts and applications to trainees who will hold jobs, such as craft, technical, and operator, which will require skills beyond basic mathematics.",
+  //         learners: "APNE, VCGNE, RFT, SST (60 hrs)",
+  //       },
+  //     ],
+  //   },
+
+  //   {
+  //     id: 3,
+  //     cover: "../images/courses/c3.png",
+  //     coursesName: "Computer Literacy & Online Security Essentials",
+  //     courseInfo: [
+  //       {
+  //         description: "This covers essential concepts and skills relating to computer devices, file management, networks, information, and online security. Learners are expected to use Microsoft (MS) Office programs for business and personal use.",
+  //         learners: "APNE, VCGNE (30 hrs)",
+  //       },
+  //     ],
+  //   },
+
+  //    {
+  //     id: 4,
+  //     cover: "../images/courses/c2.png",
+  //     coursesName: "English Functional",
+  //     courseInfo: [
+  //       {
+  //         description: "With levels ranging from 2 to 6, these courses offer foundation instruction in functional English language skills, with focus on development of productive skills (writing and speaking) that facilitate effective verbal communication accross all functional areas. Also provided are foundational instruction covering writing, typing, listening, and reading skills, with emphasis on language mastery needed to study at the ITC and to perform job tasks effectively.",
+  //         learners: "APNE, VCGNE, RFT, SST (175 hrs to 230 hrs)",
+  //       },
+  //     ],
+  //   },
+  
+  
+  // ]
 
 
   
