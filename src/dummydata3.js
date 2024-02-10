6303f704f1f256ae6d870a0071cabaa1c44bbf75
aristotleinigo9 <99675563+aristotleinigo9@users.noreply.github.com>
2024-02-10 20:50:22 +0300

    Update dummydata3.js

--- src/dummydata3.js
+++ src/dummydata3.js
@@ -1,31 +1,49 @@
+  export const coursesCard = [
+    {
+      id: 1,
+      cover: "../images/courses/c4.png",
+      coursesName: “Lorem Ipsum”,
+      courseInfo: [
+        {
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+          learners: “LOREM, IPSUM, DOLOR (90 hrs)",
+        },
+      ],
+    },
+  ]
 
-export const coursesCard = [
-  {
-    id: 1,
-    cover: "../images/courses/c8.png",
-    coursesName: "Crane Operator Certification",
-    courseInfo: [
-      {
-        description: "To test and certify crane operators on new types of cranes, grove GMK 3050, GMK 5100, GMK 5200 and Liebher LTM 1080.",
-        learners: "Regulars, Contractors (8 hrs)",
-      },
-    ],
 
-  },
 
-  {
-    id: 2,
-    cover: "../images/courses/c8.png",
-    coursesName: "Manlift Operator Certification Test",
-    courseInfo: [
-      {
-        description: "This is a test to certify operators on Manlift Operation. Operator will operate a Manlift (Hydraulic, Scissors, Telescoping). The operator will complete the tasks without error and in accordance with the Company's operating procedures and Manlift manufacturer specification.",
-        learners: "Regulars, Contractors (8 hrs)",
-      },
-    ],
 
-  },
 
-]
+
+// export const coursesCard = [
+//   {
+//     id: 1,
+//     cover: "../images/courses/c8.png",
+//     coursesName: "Crane Operator Certification",
+//     courseInfo: [
+//       {
+//         description: "To test and certify crane operators on new types of cranes, grove GMK 3050, GMK 5100, GMK 5200 and Liebher LTM 1080.",
+//         learners: "Regulars, Contractors (8 hrs)",
+//       },
+//     ],
+
+//   },
+
+//   {
+//     id: 2,
+//     cover: "../images/courses/c8.png",
+//     coursesName: "Manlift Operator Certification Test",
+//     courseInfo: [
+//       {
+//         description: "This is a test to certify operators on Manlift Operation. Operator will operate a Manlift (Hydraulic, Scissors, Telescoping). The operator will complete the tasks without error and in accordance with the Company's operating procedures and Manlift manufacturer specification.",
+//         learners: "Regulars, Contractors (8 hrs)",
+//       },
+//     ],
+
+//   },
+
+// ]
 
 
